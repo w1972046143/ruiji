77fe5b6e2974ba249ad1f5bc0d1c9c3a74520d86
xiexie <1972046143@qq.com>
2023-10-31 13:57:02 +0800

    first

---
+++ src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.itheima.reggie.common;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * annotations需要加全局异常处理方式的注解
+ */
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> expectionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error(ex.getMessage());
+
+        if(ex.getMessage().contains("Duplicate entry")){
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+        return R.error("未知错误");
+    }
+    //处理自定义异常
+    @ExceptionHandler(CustomException.class)
+    public R<String> expectionHandler(CustomException ex){
+        log.error(ex.getMessage());
+
+        return R.error(ex.getMessage());
+    }
+}
